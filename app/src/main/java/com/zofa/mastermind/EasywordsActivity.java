932b253d0e06439e9bfc651d35205847953460bb
omerktk <greatomer876@gmail.com>
2022-06-26 01:02:14 +0500

    1

---
+++ app/src/main/java/com/zofa/mastermind/EasywordsActivity.java
@@ -0,0 +1,92 @@
+package com.zofa.mastermind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EasywordsActivity extends AppCompatActivity {
+
+    MaterialButton send;
+    RequestQueue requestQueue;
+    TextView tv;
+    EditText word;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easywords);
+        word = findViewById(R.id.testword);
+
+
+        GetData();
+    }
+
+    private void GetData() {
+        requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                "http://192.168.120.108/mastermind/api/game/easy.php", null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                tv = findViewById(R.id.corword);
+                send =(MaterialButton) findViewById(R.id.send);
+
+
+                try {
+                    String a = response.getString("test");
+                    String w = response.getString("word");
+                    tv.setText(a);
+                    send.setOnClickListener(view -> {
+                        String dword = word.getText().toString();
+                        if(dword.isEmpty()){
+                            word.setError("Field Is Empty");
+                            word.requestFocus();
+                            return;
+                        }
+                        if(dword.toString().equals(w)){
+                            Toast.makeText(getApplicationContext(), "5 points added", Toast.LENGTH_LONG).show();
+                        startActivity(new Intent(getApplicationContext(),EasywordsActivity.class));
+                        }else{
+                            Toast.makeText(getApplicationContext(), "-5 points added ", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(),EasywordsActivity.class));
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(),DashboardActivity.class));
+    }
+}
--- app/src/main/java/com/zofa/mastermind/LevelsActivity.java
+++ app/src/main/java/com/zofa/mastermind/LevelsActivity.java
@@ -2,13 +2,25 @@ package com.zofa.mastermind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LevelsActivity extends AppCompatActivity {
+    MaterialButton easy,medium,hard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
+
+        easy = (MaterialButton) findViewById(R.id.easy);
+        medium = (MaterialButton) findViewById(R.id.medium);
+        hard = (MaterialButton) findViewById(R.id.hard);
+
+        easy.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(),EasywordsActivity.class));
+        });
     }
 }
