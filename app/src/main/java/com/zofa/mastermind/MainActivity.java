b8125c0c41a9da17a481bc9a548b89a25a293e3d
omerktk <greatomer876@gmail.com>
2022-07-18 15:02:52 +0500

    bilal

--- app/src/main/java/com/zofa/mastermind/MainActivity.java
+++ app/src/main/java/com/zofa/mastermind/MainActivity.java
@@ -3,15 +3,23 @@ package com.zofa.mastermind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView vv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vv = findViewById(R.id.videoView);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -19,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(x);
                 finish();
             }
-        },3000);
+        },5000);
+
+        String video_url = "android.resource://" + MainActivity.this.getPackageName()+"/" + R.raw.abcd;
+        final VideoView videoView = findViewById(R.id.videoView);
+        Uri videoUri = Uri.parse(video_url);
+        MediaController mediaController= new MediaController(MainActivity.this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(videoUri);
+        videoView.requestFocus();
+        videoView.start();
     }
 }
